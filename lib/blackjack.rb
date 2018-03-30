264de1980331757fc060427403722abc54543426
Shawn Koski <shawnnkoski@gmail.com>
2018-03-30 01:52:39 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,8 +37,15 @@ end
 
 def hit?(card_total)
   # code hit? here
-  
-
+    prompt_user
+    input = get_user_input
+    if input == "h"
+      card_total += deal_card
+    elsif input == "s"
+      card_total
+    else 
+      invalid_command
+    end
 end
 
 def invalid_command
